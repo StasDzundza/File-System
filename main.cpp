16aa568fe362555e9c7ea0b76cecc2e7a3837c23
AndrikB <blagij00@gmail.com>
2020-05-17 18:36:51 +0300

    add input parametrs for main ad input_reader

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "input_reader.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 	filesystem::InputReader inputReader;
-	inputReader.start();
+	inputReader.start(argc, argv);
 	return 0;
 }
